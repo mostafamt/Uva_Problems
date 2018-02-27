e1028c0d3b858b220700aa9ac6d93b54819b8843
Mostafa Kamel <mostafamt60@yahoo.com>
2018-02-27 19:43:50 +0200

    Add files via upload

--- Uva11631.java
+++ Uva11631.java
@@ -1,172 +1,107 @@
-
 import java.util.*;
 import java.io.*;
- 
+
 public class Uva11631
 {
-    public static void main (String[] args) throws IOException
+    public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        String s ;
-        while( (s = br.readLine()) != null && !s.isEmpty() )
+        PrintWriter pr = new PrintWriter(System.out);
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        while (n!=0&&m!=0)
         {
-            StringTokenizer st = new StringTokenizer(s);
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            if( n == 0 && m == 0 )
-                return ;
-            
-            Kruskal graph = new Kruskal(n, m);
-            int totalWeight = 0 ;
-            int index = 0 ;
-            while( m-->0 )
+            PriorityQueue<Edge> pq = new PriorityQueue<>();
+            Edge [] edgeList = new Edge[m];
+            UF uf = new UF(n+1);
+            int minCost = 0 ;
+            int maxCost = 0 ;
+            for(int i = 0 ; i < m ; i++)
             {
-                StringTokenizer str = new StringTokenizer(br.readLine());
-                int u = Integer.parseInt(str.nextToken());
-                int v = Integer.parseInt(str.nextToken());
-                int w = Integer.parseInt(str.nextToken());
-                graph.edge[index].src = u ;
-                graph.edge[index].dest = v ;
-                graph.edge[index].weight = w ;
-                totalWeight += w ;
-                index ++ ;
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                int z = Integer.parseInt(st.nextToken());
+                edgeList[i] = new Edge(x,y,z);
+                maxCost += z ;
             }
-            int w = graph.KruskalMST();
-            System.out.println( totalWeight -  w );
-            
+            Arrays.sort(edgeList);
+            for(int i = 0 ; i < m ; i++)
+            {
+                Edge e = edgeList[i];
+                int u = e.u ;
+                int v = e.v;
+                int w = e.weight;
+                if(uf.connected(u,v)) continue;
+                uf.union(u,v);
+                minCost += w ;
+            }
+            sb.append(maxCost-minCost+"\n");
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
         }
-        out.close();
+        pr.print(sb.toString());
+        pr.close();
         br.close();
     }
-    
-    
-}
+
+    static class Edge implements Comparable<Edge>
+    {
+        int u , v , weight ;
+        public Edge(int x , int y , int z)
+        {
+            u = x ; v = y ; weight = z ;
+        }
+
+        public int compareTo(Edge that)
+        {
+            if     (this.weight > that.weight) return  1 ;
+            else if(this.weight < that.weight) return -1 ;
+            else                               return  0  ;
+        }
+    }
 
 
-class Kruskal
-{
-    // A class to represent a graph edge
-    class Edge implements Comparable<Edge>
+    static class UF
     {
-        int src, dest, weight;
- 
-        // Comparator function used for sorting edges based on
-        // their weight
-        public int compareTo(Edge compareEdge)
+        private int [] id ;
+        private int count ;
+
+        public UF(int N)
         {
-            return this.weight-compareEdge.weight;
+            count = N ;
+            id = new int[N];
+            for (int i = 0; i < N; i++)
+                id[i] = i ;
+        }
+
+        public int count()
+        {
+            return count ;
+        }
+
+        public boolean connected(int p , int q)
+        {
+            return find(p) == find(q);
+        }
+
+        public int find(int p)
+        {
+            return id[p];
+        }
+
+        public void union(int p ,int q)
+        {
+            int pID = find(p);
+            int qID = find(q);
+            if(qID==pID) return ;
+            for (int i = 0; i < id.length; i++)
+                if(id[i]==pID) id[i]=qID;
+            count-- ;
         }
     }
- 
-    // A class to represent a subset for union-find
-    class subset
-    {
-        int parent, rank;
-    }
- 
-    int V, E;    // V-> no. of vertices & E->no.of edges
-    Edge edge[]; // collection of all edges
- 
-    // Creates a graph with V vertices and E edges
-    public Kruskal(int v, int e)
-    {
-        V = v;
-        E = e;
-        edge = new Edge[E];
-        for (int i=0; i<e; ++i)
-            edge[i] = new Edge();
-    }
- 
-    // A utility function to find set of an element i
-    // (uses path compression technique)
-    int find(subset subsets[], int i)
-    {
-        // find root and make root as parent of i (path compression)
-        if (subsets[i].parent != i)
-            subsets[i].parent = find(subsets, subsets[i].parent);
- 
-        return subsets[i].parent;
-    }
- 
-    // A function that does union of two sets of x and y
-    // (uses union by rank)
-    void Union(subset subsets[], int x, int y)
-    {
-        int xroot = find(subsets, x);
-        int yroot = find(subsets, y);
- 
-        // Attach smaller rank tree under root of high rank tree
-        // (Union by Rank)
-        if (subsets[xroot].rank < subsets[yroot].rank)
-            subsets[xroot].parent = yroot;
-        else if (subsets[xroot].rank > subsets[yroot].rank)
-            subsets[yroot].parent = xroot;
- 
-        // If ranks are same, then make one as root and increment
-        // its rank by one
-        else
-        {
-            subsets[yroot].parent = xroot;
-            subsets[xroot].rank++;
-        }
-    }
- 
-    // The main function to construct MST using Kruskal's algorithm
-    int KruskalMST()
-    {
-        Edge result[] = new Edge[V];  // Tnis will store the resultant MST
-        int e = 0;  // An index variable, used for result[]
-        int i = 0;  // An index variable, used for sorted edges
-        for (i=0; i<V; ++i)
-            result[i] = new Edge();
- 
-        // Step 1:  Sort all the edges in non-decreasing order of their
-        // weight.  If we are not allowed to change the given graph, we
-        // can create a copy of array of edges
-        Arrays.sort(edge);
- 
-        // Allocate memory for creating V ssubsets
-        subset subsets[] = new subset[V];
-        for(i=0; i<V; ++i)
-            subsets[i]=new subset();
- 
-        // Create V subsets with single elements
-        for (int v = 0; v < V; ++v)
-        {
-            subsets[v].parent = v;
-            subsets[v].rank = 0;
-        }
- 
-        i = 0;  // Index used to pick next edge
- 
-        // Number of edges to be taken is equal to V-1
-        while (e < V - 1)
-        {
-            // Step 2: Pick the smallest edge. And increment the index
-            // for next iteration
-            Edge next_edge = new Edge();
-            next_edge = edge[i++];
- 
-            int x = find(subsets, next_edge.src);
-            int y = find(subsets, next_edge.dest);
- 
-            // If including this edge does't cause cycle, include it
-            // in result and increment the index of result for next edge
-            if (x != y)
-            {
-                result[e++] = next_edge;
-                Union(subsets, x, y);
-            }
-            // Else discard the next_edge
-        }
- 
-        // print the contents of result[] to display the built MST
-        int weight = 0 ;
-        for (i = 0; i < e; ++i)
-            weight += result[i].weight ;
-        return weight ;
-    }
- 
-    
 }
+
