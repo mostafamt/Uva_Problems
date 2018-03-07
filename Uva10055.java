499705c606a668f0ff11dfed3091811cc72f30ba
Mostafa Kamel <mostafamt60@yahoo.com>
2018-03-07 19:54:02 +0200

    Add files via upload

--- Uva10055.java
+++ Uva10055.java
@@ -1,37 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author mostafa
- */
+import java.util.*;
+import java.io.*;
 
-import java.util.* ;
-import java.io.* ;
-
-public class Uva10055 
+public class Uva10055
 {
-    
-    public static void main(String[] args) throws IOException 
+
+    public static void main(String[] args) throws IOException
     {
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
-        PrintWriter out = new PrintWriter(System.out);
-        
-        String s ;
-        while( (s = br.readLine())!= null && !s.isEmpty() )
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pr = new PrintWriter(System.out);
+        StringBuilder sb = new StringBuilder();
+        String str = "" ;
+        while( (str = br.readLine())!=null )
         {
-            StringTokenizer st = new StringTokenizer(s);
-            long a = Long.parseLong(st.nextToken());
-            long b = Long.parseLong(st.nextToken());
-            
-            out.println( (long)Math.abs(a-b) );
+            StringTokenizer st = new StringTokenizer(str);
+            sb.append(Math.abs(Long.parseLong(st.nextToken())-Long.parseLong(st.nextToken()))).append("\n");
         }
-        
+        pr.print(sb.toString());
+        pr.close();
         br.close();
-        out.close();
     }
-    
+
+
+
 }
