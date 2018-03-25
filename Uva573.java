3304e0b58e4a260a14040a5834c8b59eb97494f9
Mostafa Kamel <mostafamt60@yahoo.com>
2018-03-25 18:46:25 +0200

    Add files via upload

---
+++ Uva573.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Uva573
+{
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pr = new PrintWriter(System.out);
+        StringBuilder sb = new StringBuilder();
+        while(br.ready())
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double height = Integer.parseInt(st.nextToken());if(height==0)break;
+            double distanceClimbed = Integer.parseInt(st.nextToken());
+            int distanceSlid = Integer.parseInt(st.nextToken());
+            int factor = Integer.parseInt(st.nextToken());
+            double decreaseFactor = (double) factor / 100 * distanceClimbed ;
+            boolean status = false ;
+            int day = 0 ;
+            double initialHeight = 0 ;
+            while(initialHeight <= height && initialHeight >= 0)
+            {
+                day++ ;
+                initialHeight += distanceClimbed ;
+                if(initialHeight>height)
+                {
+                    status = true ;
+                    break;
+                }
+                initialHeight -= distanceSlid ;
+                if(distanceClimbed>0)
+                    distanceClimbed -= decreaseFactor ;
+            }
+            sb.append(status ? "success":"failure").append(" on day ").append(day).append("\n");
+        }
+        pr.print(sb.toString());
+        pr.close();
+        br.close();
+    }
+
+}
