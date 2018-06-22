d33c02665ab78a53f243b85a9ec3c1d6e4d9a100
Mostafa Kamel <mostafamt60@yahoo.com>
2018-06-22 09:23:44 +0200

    add uva575.cpp

---
+++ uva575.cpp
@@ -0,0 +1,26 @@
+/* @author : mostafa
+ * created : 2018-06-22 09:07
+ */
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std ;
+
+int main()
+{
+	cin.sync_with_stdio(false) ;
+	cin.tie(0) ;
+    string s ;
+    while( getline( cin , s ) ,  s != "0" ){
+        int size = s.size();
+        int sum = 0 ;
+        for( int i = 0 ; i < size ; i++ ){
+            sum += ( s[i] - '0' ) * ( pow(2,size-i) - 1 ) ;
+        }
+        cout << sum << "\n" ;
+        s.clear();
+    }
+	return 0 ;
+}
+
+
